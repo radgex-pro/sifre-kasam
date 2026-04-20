76dc0045e703867c34315a8249fad126c5c14fbd
radgex-pro <kutluhanerkoyun@gmail.com>
2026-04-20 19:27:32 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-  <rect width="512" height="512" rx="100" fill="#0a0a0f"/>
-  <rect x="40" y="40" width="432" height="432" rx="80" fill="#13131a"/>
-  <text x="256" y="320" font-size="220" text-anchor="middle" font-family="serif">🔐</text>
-  <rect x="60" y="400" width="392" height="6" rx="3" fill="#00ff88" opacity="0.5"/>
-</svg>
